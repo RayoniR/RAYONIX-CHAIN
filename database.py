c473fc1180bb61fd0b1ef7f32ca9ec6dba6f97b3
infortechray <rayonireuben@duck.com>
2025-09-12 00:05:43 +0300

    Update database.py

--- database.py
+++ database.py
@@ -24,6 +24,17 @@ import mmap
 import numpy as np
 from bloom_filter import BloomFilter
 import crc32c
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)  # You can set DEBUG, WARNING, etc.
+
+# Optional: add console output
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 class DatabaseType(Enum):
     PLYVEL = auto()
