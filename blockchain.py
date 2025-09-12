f61aad08662b37d4a931e46037943662a03ddce8
infortechray <rayonireuben@duck.com>
2025-09-12 08:53:38 +0300

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -12,7 +12,7 @@ from cryptography.exceptions import InvalidSignature
 from merkle import MerkleTree
 from utxo import UTXOSet, Transaction, UTXO
 from consensus import ProofOfStake, Validator
-from database import BlockchainDB #IndexManager
+from database import AdvancedDatabase
 from smart_contract import ContractManager, SmartContract
 from config import get_config
 self.config_manager = get_config()
